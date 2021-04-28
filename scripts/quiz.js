32cd09575c74f1764f83dcf2f258ca99456d464d
ivorymash <checkrflix@yahoo.com>
2021-04-29 03:26:27 +0800

    added question type 2

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,8 +1,7 @@
 var json = require('sampleQns.json');
-var selectedAns = "";
+var qnType = "";
+var selectedAns = ""; //catch all variable for the answer picked/typed/whatever.
 var correctAns = "";
-
-//IMPORTANT: next time there will be button generation through code, so the number hardcodede into the button functions will change. 
 
 function stuff() {
     $.getJSON("sampleQns.json"), function (data) {
@@ -23,7 +22,7 @@ function getData() {
             return response.json();
         })
         .then(function (myJson) {
-            loadQuestion(myJson["Questions"][0])
+            loadQuestion(myJson["Questions"][1])
         });
 }
 useEffect(() => {
@@ -34,31 +33,101 @@ useEffect(() => {
 function loadQuestion(data) {
     //TODO: add support for question types
     //TODO: spawn buttons based on how many answeres there are.
-    text = data.Question;
-    document.getElementById("qnText").innerHTML = data.Question
-    buttons = document.getElementsByClassName("answer");
-    correctAns = data.Correct;
-    for (i = 0; i < data.Answers.length; i++) {
-        document.getElementsByClassName("answer")[i].innerHTML = data.Answers[i];
+    switch (data.QnsType) { //TODO: add the other question types here
+        case "1":
+            loadQnType1(data);
+            break;
+
+        case "2":
+            loadQnType2(data);
+            break;
+
+        default:
+            break;
+
     }
+}
+
+function loadQnType1(data) {
+
+    correctAns = data.Correct; //store the correct answer
+
+    qnType = "1";
+
+    var qnType1 = `<h3 id="qnText">${data.Question}</h3>
+    <div>`
+    for (i = 0; i < data.Answers.length; i++) {
+        qnType1 += `<button onclick="select('${i + 1}')" class="answer">${data.Answers[i]}</button>`
+    }
+    qnType1 += `</div> <h2 id="selectedAns">bruh</h2>`;
+
+    document.getElementById("qnBody").innerHTML += qnType1;
+    console.log("appending type 1 qn");
+}
+
+function loadQnType2(data) {
+
+    qnType = "2";
+
+    correctAns = data.AcceptedAns; //store the correct answer
+
+    console.log(correctAns);
+
+    console.log("loading qn type 2")
+    var qnType2 = `<h3 id="qnText">${data.Question}</h3>
+    <div>
+    <input type="text" id="selectedAns" name="answerField"><br>
+
+  </form></div>`
+
+
+    document.getElementById("qnBody").innerHTML += qnType2;
+
 }
 
 
 function select(number) {
     console.log(number + " selected")
     selectedAns = number;
+    document.getElementById("selectedAns").innerHTML = number;
 }
 
 
 function lockQuestion() {
     console.log(selectedAns + correctAns);
 
+    //read the current question type, then pass judgement.
+
+    switch (qnType) {
+        case "1":
+            verifyQnType1();
+            break;
+        case "2":
+            selectedAns = document.getElementById("selectedAns").value;
+            console.log(selectedAns);
+            verifyQnType2();
+            break;
+    }
+}
+
+function verifyQnType1() {
     if (selectedAns == correctAns) {
 
         alert("right");
 
     } else {
-
         alert("wrong");
     }
+}
+
+function verifyQnType2() {
+    var isCorrect = false;
+
+    for (i = 0; i < correctAns.length; i++) {
+        if (correctAns[i] == selectedAns) {
+            isCorrect = true;
+        }
+    }
+
+    isCorrect ? alert("right") : alert("wrong")
 }

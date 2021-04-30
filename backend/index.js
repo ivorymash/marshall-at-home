b858cf5ae74267e1afacded0b653186e350df898
IWillSmackYou <lowbingxuan2016@gmail.com>
2021-04-30 21:30:50 +0800

    Backend Update

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,42 @@
+const mysql = require('mysql2');  
 var express = require("express");
 var app = express();
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+const bodyparser = require('body-parser'); 
+require('dotenv').config();
+const env = process.env;
+
+app.use(bodyparser.json());  
+
+// Connection String to Database  
+var mysqlConnection = mysql.createConnection({  
+    host: env.DB_HOST,  
+    user : env.DB_USER,  
+    port: 3306,
+    password : env.DB_PASSWORD,   
+    database : env.DB_NAME,  
+    multipleStatements : true  
+});  
+
+mysqlConnection.connect((err) => {  
+    if(!err) {  
+        console.log("Db Connection Succeed");  
+    }  
+    else{  
+        console.log("Db connect Failed \n Error :" + JSON.stringify(err,undefined,2));  
+    }  
+});  
+
+app.listen(env.SERVER_PORT,()=> console.log("Express server is running at port no : " + env.SERVER_PORT)); 
+
+app.get('/users/all',(req,res)=>{  
+    mysqlConnection.query('SELECT * FROM users',(err,rows,fields)=>{  
+    if(!err)   
+        res.send(rows);  
+    else  
+        console.log(err);  
+      
+    })  
+}); 
 
 //test api
 app.get("/test", (req, res, next) => {
